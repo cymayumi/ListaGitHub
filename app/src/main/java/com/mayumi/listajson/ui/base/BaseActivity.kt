bee4fce09203e7d5bc80cca59838105b4b3f7cf9
Cynthia Oka <cynthia.mosilva@gmail.com>
2020-08-07 20:36:50 -0300

    Cores layout

--- app/src/main/java/com/mayumi/listajson/ui/base/BaseActivity.kt
+++ app/src/main/java/com/mayumi/listajson/ui/base/BaseActivity.kt
@@ -1,14 +1,19 @@
 package com.mayumi.listajson.ui.base
 
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.mayumi.listajson.R
 
 
 open class BaseActivity : AppCompatActivity() {
 
     protected fun setUpToolbar(toolbar: Toolbar, titleIdRes: Int, showBackButton: Boolean = false) {
-        toolbar.title = getString(titleIdRes)
+        val titleTool = findViewById<TextView>(R.id.title_toolbar)
+        titleTool.text = getString(titleIdRes)
+
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         if (showBackButton) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
